f5ea00f6311f3120da23fadaabf93c4712609081
root <root@015a73ea38c1>
2020-10-04 23:17:01 +0000

    amazing rings and happy pet

--- src/_02_nested_loops/_2_amazing_rings/amazing_rings/amazing_rings.pde
+++ src/_02_nested_loops/_2_amazing_rings/amazing_rings/amazing_rings.pde
@@ -2,7 +2,7 @@ int x = 250;
 int y = 250;
 int speed = 2;
 int x2 =  300;
-int y2 = 300;
+int y2 = 250;
 int speed2 = -2;
 void setup() {
   size(500, 500);
@@ -17,6 +17,13 @@ void draw() { background(150);
   x += speed;
    for(int i=0; i<10; i++){ ellipse(x2,y2, i*10, i*10);}
   x2 += speed2;
+    
+  if(x > width || x < 0){
+   speed = -speed; 
+  }
+    if(x2 > width || x < 0){
+   speed2 = -speed2; 
+  }
   
   /*
   Use the code from your Bullseye program to draw the rings shown in this recipe.
@@ -24,4 +31,5 @@ void draw() { background(150);
   Make the rings move past each other.
   When the rings hit the side of the sketch, make them reverse their direction.
   */
+  
 }
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -1,30 +1,55 @@
-=package _03_method_writing._2_happy_pet;
+package _03_method_writing._2_happy_pet;
 import javax.swing.JOptionPane;
 
 public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	String happyiness = 
+	static int happinessLevel =  0; 
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
+String pet = JOptionPane.showInputDialog("what kind of pet would like to buy?");
 
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "How are you gonna take car your pet", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Walk it every day", "Feed it food", "play with your pet" }, null);
 
+			System.out.println(task);
 			// 5. Use user input to call the appropriate method created in step 4.
-
+			if(task == 0) {
+				walkPet();
+			}
+			if(task == 1 ) {
+				feedPet();
+			}
+			
+			if(task == 2) {
+				playwithpet();
+			}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
 	}
 
+	private static void playwithpet() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void feedPet() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	static void walkPet() {
+		JOptionPane.showMessageDialog(null, "Thank you for walking me!");
+		happinessLevel += 10;
+	}
 }
